21d1dc73e13edca7cca475fed49427dd741035ce
Mykhailo Levandovskyi <mlevandovskyi@softjourn.com>
2017-09-06 19:11:49 +0300

    ddd

---
+++ test.sh
@@ -0,0 +1,8 @@
+# Setup
+git config git-ftp.url "ftp://demomaegan.co/tmp/"
+git config git-ftp.user "mlevandovskyi@demomaegan.co"
+git config git-ftp.password "sds35v"
+
+# Upload all files
+git ftp init
+
