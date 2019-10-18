edeeec4797f34759ea4bdc7ec49791f34c2864f3
9rcrsz <9rcrsz@gmail.com>
2019-10-18 16:35:09 +0300

    save

--- test.sh
+++
@@ -1,9 +0,0 @@
-# Setup
-git config git-ftp.url "ftp://demomaegan.co/tmp/"
-git config git-ftp.user "mlevandovskyi@demomaegan.co"
-git config git-ftp.password "sds35v"
-
-# Upload all files
-git ftp init
-
-git ftp push
